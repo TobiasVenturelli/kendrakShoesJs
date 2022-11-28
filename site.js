243300705530b5e533faaceef761743e09d76715
tobias venturelli <venturelliquimey@gmail.com>
2022-11-28 18:19:26 -0300

    Primera pre-entrega

--- site.js
+++ site.js
@@ -1,2 +1,18 @@
-let nombre
-let numero  
+let nombre = prompt("Por favor, ingrese su nombre...");
+if (nombre == ""){
+    alert("No ingresaste nada");
+}
+ else {
+    alert("Hola " + nombre);
+    console.log ("Hola " + nombre)
+ }
+
+ let precio = prompt ("Cual es el monto que quieres verificar con el descuento?");
+ alert ("ingresaste " + precio);
+
+const descuento = 0.15
+const resta = (a,b) => { return a - b }
+const calcDescuento = x => x * descuento;
+
+let nuevoPrecio = alert("El precio con el descuento es " + resta(precio, calcDescuento(precio)));
+console.log(nuevoPrecio);
