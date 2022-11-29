a7a7a8b60558f7514b8303c90533c0d8a956db7d
tobias venturelli <venturelliquimey@gmail.com>
2022-11-29 13:34:58 -0300

    Terminado primera pre-entrega

--- site.js
+++ site.js
@@ -12,11 +12,23 @@ alert("ingresaste " + precio);
 
 const iva = 0.21
 const descuento = 0.15
-const suma  = (a,b) => { return a + b }
-const resta = (a, b) => { return a - b }
-const calcDescuento = x => x * descuento;
-const calcIva = x => x * iva;
+const suma  = function (a,b) { return a + b }
+const resta = function (a,b) { return a - b }
+const calcDescuento = function (x) {return x * descuento};
 
-let nuevoPrecio = alert("El precio con el descuento es " + resta(precio, calcDescuento(precio)));
-console.log("El precio con el descuento es " + resta(precio, calcDescuento(precio)));
+let nuevoPrecio = resta(precio, calcDescuento(precio));
+alert("El precio con el descuento es " + nuevoPrecio)
+console.log(nuevoPrecio);
 
+const calcIva = function (a,b) {return nuevoPrecio * iva};
+
+let respuesta = prompt(
+    "¿Quieres verificar el precio con IVA?\n si\n no\n")
+    console.log (respuesta)
+
+    function calcularIva(){
+        if (respuesta == "si"){
+            let precioConIva = resta(nuevoPrecio, calcIva);
+            console.log(precioConIva)
+        }
+    }
