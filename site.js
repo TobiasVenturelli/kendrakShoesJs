b27a7134a01a4f9282da48f42dbb24ccadb08c7e
tobias venturelli <venturelliquimey@gmail.com>
2022-11-29 04:36:11 -0300

    Retoques para la primer pre-entrega

--- site.js
+++ site.js
@@ -1,18 +1,22 @@
 let nombre = prompt("Por favor, ingrese su nombre...");
-if (nombre == ""){
+if (nombre == "") {
     alert("No ingresaste nada");
 }
- else {
+else {
     alert("Hola " + nombre);
-    console.log ("Hola " + nombre)
- }
+    console.log("Hola " + nombre)
+}
 
- let precio = prompt ("Cual es el monto que quieres verificar con el descuento?");
- alert ("ingresaste " + precio);
+let precio = prompt("Cual es el monto que quieres verificar con el descuento?");
+alert("ingresaste " + precio);
 
+const iva = 0.21
 const descuento = 0.15
-const resta = (a,b) => { return a - b }
+const suma  = (a,b) => { return a + b }
+const resta = (a, b) => { return a - b }
 const calcDescuento = x => x * descuento;
+const calcIva = x => x * iva;
 
 let nuevoPrecio = alert("El precio con el descuento es " + resta(precio, calcDescuento(precio)));
-console.log(nuevoPrecio);
+console.log("El precio con el descuento es " + resta(precio, calcDescuento(precio)));
+
